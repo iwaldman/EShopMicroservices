6aab57026781943f02e8226fb3ea2ce6d58644cd
iwaldman <irvin.waldman@hotmail.com>
2024-08-10 09:28:04 -0500

    Add Basket API endpoints and handlers

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,5 +1,19 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketHandler
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart Cart);
+
+internal class GetBasketHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
+    public async Task<GetBasketResult> Handle(
+        GetBasketQuery query,
+        CancellationToken cancellationToken
+    )
+    {
+        // Get basket from DB
+        // var basket = await _repository.GetBasketAsync(query.UserName);
+
+        return new GetBasketResult(new ShoppingCart { UserName = "swn" });
+    }
 }
---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart cannot be null.");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required.");
+    }
+}
+
+internal class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(
+        StoreBasketCommand command,
+        CancellationToken cancellationToken
+    )
+    {
+        // TODO: store basket in DB (use Marten upsert)
+        // TODO: update cache
+
+        return new StoreBasketResult("swn");
+    }
+}
